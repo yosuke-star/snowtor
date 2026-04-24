6088367e102762e73b675c59b6b8fe530018ba4c
yosuke <bigstart122091@gmail.com>
2026-04-24 13:39:48 +0900

    fix: common file and refactoring

---
+++ common/form_utils.py
@@ -0,0 +1,14 @@
+COMMON_INPUT_CLASS = '''
+    mt-1
+    block
+    w-full
+    px-3
+    py-2
+    border
+    border-gray-300
+    rounded-md shadow-sm
+    focus:outline-none
+    focus:ring-indigo-500
+    focus:border-indigo-500
+    sm:text-sm
+'''.strip()
--- dashboard_app/forms.py
+++ dashboard_app/forms.py
@@ -1,21 +1,6 @@
 from django import forms
+from common.form_utils import COMMON_INPUT_CLASS
 from .models import ActivityType, LessonDetail, Prefecture, SkiResort
-
-# 共通のCSSクラス
-COMMON_INPUT_CLASS = '''
-    mt-1
-    block
-    w-full
-    px-3
-    py-2
-    border
-    border-gray-300
-    rounded-md shadow-sm
-    focus:outline-none
-    focus:ring-indigo-500
-    focus:border-indigo-500
-    sm:text-sm
-'''
 
 def date_input_widget():
     return forms.DateInput(attrs={
